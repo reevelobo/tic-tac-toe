40496729385735158575697d900f7fabe65cebea
reevelobo <reevelobo@gmail.com>
2024-05-10 14:03:04 +0530

    Added image to dockerHub repository

--- dockercommands.sh
+++ dockercommands.sh
@@ -3,3 +3,7 @@ docker ps -a
 docker build -t tic-tac-toe .
 docker run -p 5173:5173 react-docker 
 docker run -p 5173:5173 -v "$(pwd):/app" -v  /app/node_modules tic-tac-toe
+
+# Push to Docker hub
+docker tag tic-tac-toe:latest reeveignatius/tic-tac-toe:latest
+docker push reeveignatius/tic-tac-toe:latest
